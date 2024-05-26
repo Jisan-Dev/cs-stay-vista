2e3ebba370d9587b483c057d1cce1cec9b6573f9
Jisun696 <istiakkashem35@gmail.com>
2024-05-27 02:26:08 +0600

    added active category style

--- client/src/components/Categories/CategoryBox.jsx
+++ client/src/components/Categories/CategoryBox.jsx
@@ -1,8 +1,11 @@
 import PropTypes from 'prop-types';
 import queryString from 'query-string';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 const CategoryBox = ({ label, icon: Icon }) => {
+  const [params, _setParams] = useSearchParams();
+  const category = params.get('category');
+  const isCategoryMatched = category === label;
   const navigate = useNavigate();
   const handleClick = () => {
     let currentQuery = { category: label };
@@ -16,7 +19,11 @@ const CategoryBox = ({ label, icon: Icon }) => {
   };
 
   return (
-    <div onClick={handleClick} className={`flex flex-col items-center justify-center  gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer`}>
+    <div
+      onClick={handleClick}
+      className={`flex flex-col items-center justify-center  gap-2 p-3 border-b-2 hover:text-neutral-600 transition cursor-pointer ${
+        isCategoryMatched && 'border-b-neutral-600 text-neutral-600'
+      } `}>
       <Icon size={26} />
       <div className="text-sm font-medium">{label}</div>
     </div>
