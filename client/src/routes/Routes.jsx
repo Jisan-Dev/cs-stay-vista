f8f21f940f9cab80b12052a75fe8bcb7823a9b3f
Jisun696 <istiakkashem35@gmail.com>
2024-07-06 17:21:02 +0600

    added profile page

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -10,6 +10,7 @@ import DashboardLayout from '../layouts/DashboardLayout';
 import Statistics from '../pages/Dashboard/Common/Statistics';
 import AddRoom from '../pages/Dashboard/Host/AddRoom';
 import MyListings from '../pages/Dashboard/Host/MyListings';
+import Profile from '../pages/Dashboard/Common/Profile';
 
 export const router = createBrowserRouter([
   {
@@ -56,6 +57,10 @@ export const router = createBrowserRouter([
       {
         path: '/dashboard/my-listings',
         element: <MyListings />,
+      },
+      {
+        path: '/dashboard/profile',
+        element: <Profile />,
       },
     ],
   },
