a788103a9ab896c70a7bb487a26ad594f5d9b1ca
Jisun696 <istiakkashem35@gmail.com>
2024-07-09 15:48:49 +0600

    BookingModal componnt added

--- client/src/pages/RoomDetails/RoomDetails.jsx
+++ client/src/pages/RoomDetails/RoomDetails.jsx
@@ -60,28 +60,12 @@ const RoomDetails = () => {
             {/* Room Info */}
             <div className="col-span-4 flex flex-col gap-8">
               <div className="flex flex-col gap-2">
-                <div
-                  className="
-                text-xl 
-                font-semibold 
-                flex 
-                flex-row 
-                items-center
-                gap-2
-              ">
+                <div className="text-xl font-semibold flex flex-row items-center gap-2">
                   <div>Hosted by {room?.host?.name}</div>
 
                   <img className="rounded-full" height="30" width="30" alt="Avatar" src={room?.host?.image} referrerPolicy="no-referrer" />
                 </div>
-                <div
-                  className="
-                flex 
-                flex-row 
-                items-center 
-                gap-4 
-                font-light
-                text-neutral-500
-              ">
+                <div className="flex flex-row items-center gap-4 font-light text-neutral-500">
                   <div>{room?.guests} guests</div>
                   <div>{room?.bedrooms} rooms</div>
                   <div>{room?.bathrooms} bathrooms</div>
@@ -89,11 +73,7 @@ const RoomDetails = () => {
               </div>
 
               <hr />
-              <div
-                className="
-          text-lg font-light text-neutral-500">
-                {room?.description}
-              </div>
+              <div className="text-lg font-light text-neutral-500">{room?.description}</div>
               <hr />
             </div>
 
